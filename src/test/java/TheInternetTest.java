ab8cea27c15a56ba630dc9f3dcef5e8c8381ddd9
ppilus <pawel.pilus@gmail.com>
2021-01-24 04:41:12 +0100

    pierwsze testy - poprawka na wszystkie przechodzace

--- src/test/java/TheInternetTest.java
+++ src/test/java/TheInternetTest.java
@@ -49,8 +49,7 @@ public class TheInternetTest {
 
     @Test()
     public void testWindow() {
-        System.out.println("Test");
-        expectedResult = "New hWindow";
+        expectedResult = "New Window";
         errorMessage = "Blad powrotu do okna glownego w metodzie - testWindow";
         assertEquals(theInternet.getNewWindow(), expectedResult, errorMessage);
     }
