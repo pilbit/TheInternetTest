ab4f0eb9bcd16a04249e58dc8c6a485fe18c1919
ppilus <pawel.pilus@gmail.com>
2021-01-26 00:55:57 +0100

    testy fun internet - Basic Auth

--- src/test/java/source/pom/BasicAuth.java
+++ src/test/java/source/pom/BasicAuth.java
@@ -1,6 +1,5 @@
 package source.pom;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
